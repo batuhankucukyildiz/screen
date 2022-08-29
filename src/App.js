d0051927177ad3c0dd56227d99f18c50b5ba7271
Batuhan <32312869+batuhankucukyildiz@users.noreply.github.com>
2022-08-29 21:51:38 +0300

    comt

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import "./style.css";
-
 import "bootstrap/dist/css/bootstrap.min.css";
 {
   /* Bootstrap kütüphanesi dahil edildi. */
 }
 
-const App = ({ information, generalnumber }) => {
+const App = ({information,generalnumber}) => {
+
+  const dizi = {information};
+  const { name, ...kalanlar } = dizi; // (...) rest/spread operatörüdür.
+
   return (
-    <Card className="stylecss">
-      <Card.Body>{props.information}</Card.Body>
-      <Card.Title>{props.generalnumber}</Card.Title>
-    </Card>
+   <h2 className="btk">{information}
+    
+    <img src={generalnumber} alt="" className="imgstyle" />
+   </h2>
+
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -7,10 +7,8 @@ const User = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    const fecthData = async () => {
-      const key = process.env.API_KEY;
-      const response = await axios.get(
-        `https://jsonplaceholder.typicode.com/comments`
+      const fecthData = async () => {
+      const response = await axios.get(`https://630c989853a833c5342f7d3b.mockapi.io/users`
       );
       setData(response.data);
     };
@@ -22,8 +20,9 @@ const User = () => {
       <ul>
         {data.map((repo) => (
           <div key={repo.id}>
-            <App information={repo.email} />
-            <App generalnumber={repo.id} />
+            
+            <App information={repo.name} />
+            <App generalnumber={repo.avatar} />
           </div>
         ))}
       </ul>
