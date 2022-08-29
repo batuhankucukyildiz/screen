b6a93821b9ee3e6a500adab08ee855aebe950138
Batuhan <32312869+batuhankucukyildiz@users.noreply.github.com>
2022-08-29 09:17:15 +0300

    react_app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect } from "react";
+import ReactDom from "react-dom";
+import axios from "axios";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const User = () => {
+  const [data, setData] = useState([]);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  useEffect(() => {
+    const fecthData = async () => {
+      const key = process.env.API_KEY;
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/comments`
+      );
+      setData(response.data);
+    };
+    fecthData();
+  }, []);
+
+  return (
+    <div className="container">
+      <ul>
+        {data.map((repo) => (
+          <div key={repo.id}>
+            <App information={repo.email} />
+            <App generalnumber={repo.id} />
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+ReactDom.render(<User />, document.querySelector("#root"));
